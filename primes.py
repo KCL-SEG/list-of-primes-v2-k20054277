c73bc052468220218ed1d7848c2b8a81fce55f11
jackie <k20054277@kcl.ac.uk>
2022-10-13 08:45:48 +0100

    try1

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    
+    if number_of_primes >= 1:
+        list = [2]
+        while (len(list) < number_of_primes):
+            testNum = 3
+            for i in range(2, testNum//2):
+                if (testNum % i) == 0:
+                    break
+                else :
+                    list.append(testNum)
+    else:
+        raise ValueError
+
     return list
