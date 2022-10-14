29dfed0987098bfc321f8db358445dc68e7d74bf
jackie <k20054277@kcl.ac.uk>
2022-10-14 14:31:55 +0100

    passed checks

--- primes.py
+++ primes.py
@@ -5,13 +5,18 @@ def primes(number_of_primes):
     
     if number_of_primes >= 1:
         list = [2]
+        testNum = 3
+        
         while (len(list) < number_of_primes):
-            testNum = 3
-            for i in range(2, testNum//2):
+            prime = True
+            for i in range(2, testNum):
                 if (testNum % i) == 0:
+                    prime = False
                     break
-                else :
-                    list.append(testNum)
+                
+            if prime == True:
+                list.append(testNum)
+            testNum= testNum+1
     else:
         raise ValueError
 
